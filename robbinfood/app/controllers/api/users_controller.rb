abbdaef51c669acfd9ee44a1c0fd7246ec43fdf5
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-07-29 14:30:44 -0700

    finisheduserscontroller

--- robbinfood/app/controllers/api/users_controller.rb
+++ robbinfood/app/controllers/api/users_controller.rb
@@ -1,3 +1,39 @@
-class UsersController < ApplicationController
-  
+class Api::UsersController < ApplicationController
+    def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render json: 'Creation Successful'
+    else
+      render json: 'Failed'
+    end
+  end
+
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+
+  private
+
+  def user_params
+    # params.require(:user).permit(:email, :email)
+    # Add password
+    params.require(:user).permit(
+      :email, 
+      :password, 
+      :username,
+      :first_name,
+      :last_name,
+      :dob,
+      :portfolio_value)
+  end
+
 end
--- robbinfood/app/models/user.rb
+++ robbinfood/app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, presence: true
+  validates :username, :first_name, :last_name, :dob, :portfolio_value, presence: true 
+
+  attr_reader :password 
+
+  after_initialize :ensure_session_token 
+
+  def sef.find_by_credentials(email=null, username=null, password)
+    username == null ? user = User.find_by(email: email) : user = User.find_by(username: usernmae)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.pasword_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save! 
+    self.session_token 
+  end
+
+  private
+
+  def ensure_session_token
+    self.ssesion_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end
