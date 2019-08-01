5bb79dcf360ab5f5ce6930a0847bd5d98f3bc38a
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-08-01 15:18:17 -0700

    fixednavbaroptionsofuserauth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 
-import NavBar from './nav_bar/nav_bar'
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+
+import RootNavBarContainer from './nav_bar/nav_bar_root_container'
+import SignUpFormContainer from "./session_form/signup_form_container";
+import LogInFormContainer from "./session_form/login_form_container";
+import CashManagement from "./extra_features/cash_mng";
+import Blog from "./extra_features/blog";
+import Careers from "./extra_features/careers";
+import Help from "./extra_features/help";
+import Investing from "./extra_features/investing";
+import Snacks from "./extra_features/snacks";
+
 const App = () => (
   <div> 
-    <NavBar />
+    <RootNavBarContainer />
+
+    <Switch>
+      <Route exact path="/snacks" component={Snacks} />
+      <Route exact path="/blog" component={Blog} />
+      <Route exact path="/help" component={Help} />
+      <Route exact path="/careers" component={Careers} />
+      <Route exact path="/investing" component={Investing} />
+      <Route exact path="/cashmng" component={CashManagement} />
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+    </Switch>
   </div>
 )
 
---
+++ frontend/components/nav_bar/nav_bar_root.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PreLogNav from './nav_bar_prelog';
+import PostLogNav from './nav_bar_postlog'
+
+const RootNavBar = ({ currentUser }) => {
+  const newNav = () => (
+    <PreLogNav />
+  );
+  const loggedInNav= () => (
+    <PostLogNav/>
+  );
+
+  return currentUser ? newNav() : loggedInNav();
+};
+
+export default RootNavBar
