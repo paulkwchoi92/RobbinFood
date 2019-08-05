a0ec581aa07e8a3406f7553e3efded9a1f0fb392
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-08-05 15:16:53 -0700

    finishingmvpss

--- frontend/robbinfood.jsx
+++ frontend/robbinfood.jsx
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // window.logIn = logIn;
   // window.logOut = logOut;
   // window.signUp = signUp;
-  // window.getState = store.getState;
+  window.getState = store.getState;
   // window.dispatch = store.dispatch;
   // window.fetchBenches = fetchBenches;
 });
