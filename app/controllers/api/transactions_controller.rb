82830b8afaef268d0a8fa8cc24a73fe923d66d54
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-08-07 12:55:34 -0700

    slightfixforreview

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,16 +1,6 @@
 class Api::TransactionsController < ApplicationController 
   def create
-    @transaction = Transaction.new(transaction_params)
-    @user = User.find(@transaction.user_id)
-    total_price = @transaction.stock_price * @trasnaction.num_shares
-    if @transaction.transaction_type == "purchase"
-      if @user.buying_power < total_price 
-        render json: ["Insufficient Funds"]
-      else
-        @user.buying_power -= total_price 
-      end
-    elsif @transaction.transaction_type == "sale"
-      if @user.
+  end
 
 
  end
