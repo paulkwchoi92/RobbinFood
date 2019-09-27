99af35298bf99448506cbc0f15c1503211f94729
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-09-27 06:06:45 -0700

    fixing branch

--- frontend/actions/stock_actions.js
+++ frontend/actions/stock_actions.js
@@ -1,4 +1,25 @@
-import * as StockAPIUtil from "../utils/stock_api_utils";
+
+
+
+import * as StockAPIUtil from "../util/stock_api_util";
+
+export const RECEIVE_ONE_STOCK = "RECEIVE_ONE_STOCK";
+export const RECEIVE_MANY_STOKCS = "RECEIVE_MANY_STOCKS"
+export const RECEIVE_STOCK_ERRORS = "RECEIVE_STOCK_ERRORS"
+export const receiveOneStocker = stock => ({
+  type: RECEIVE_ONE_STOCK,
+  stock
+})
+
+export const receiveManySotkcs = stocks => ({
+  type: RECEIVE_MANY_STOKCS,
+  stocks
+})
+
+export const receiveStockErrors = errors => ({
+  type: RECEIVE_STOCK_ERRORS,
+  errors
+})
 
 
 
