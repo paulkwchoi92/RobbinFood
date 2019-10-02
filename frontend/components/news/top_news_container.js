74b0d503a1cb9465e3a27d9cf5c712669f2835de
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-02 16:38:24 -0700

    fetch topnews container

--- frontend/components/news/top_news_container.js
+++ frontend/components/news/top_news_container.js
@@ -1,1 +1,21 @@
-import { connect} from 'react-redux'
+import { connect } from 'react-redux'
+
+
+import News from "./news"
+import { fetchTopNews } from '../../actions/news_actions'
+
+const mapStateToProps = ({ session, entities: { users } }) => {
+  return {
+    currentUser: users[session.id]
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(fetchCompanyNews())
+});
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(News);
