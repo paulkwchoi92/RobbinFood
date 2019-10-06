a67932451e5356c411d201573f45d9582d2d98bc
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-06 10:50:21 -0700

    trasansaction error

--- frontend/components/stocks/stocks_transaction.jsx
+++ frontend/components/stocks/stocks_transaction.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class StocksTransaction extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { 
+    super(props);
+    this.state = {
       switch: false,
       numOfShares: 0,
       error: ""
-
-    }
+    };
   }
   renderEstimatedCost() {
-    return (<div>{this.state.numOfShares * this.props.marketPrice}</div>)
+    return <div>{this.state.numOfShares * this.props.marketPrice}</div>;
   }
-  
+
   renderReviewButton() {
     return (
       <div>
         <button>Review Order</button>
       </div>
-    )
+    );
   }
   renderErrors() {
     return (
       <div className="transaction-error-container">
-        <div className="transaction-error-header"></div>
+        <div className="transaction-error-header">
+          <i class="fas fa-exclamation-circle"></i>
+          Error
+        </div>
+        <div className="transaction-error-msg">
+          {this.state.error}
+        </div>
       </div>
-    )
+    );
   }
 
-  render
+  render;
 
   render() {
-    return ( 
-      <div> 
-
-      </div>
-    )
+    return <div></div>;
   }
 }
