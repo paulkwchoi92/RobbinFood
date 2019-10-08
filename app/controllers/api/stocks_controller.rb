707c52494b19d48e86cff0bac79313bb124df6c1
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-07 20:12:28 -0700

    adding different dates

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -6,6 +6,9 @@ class Api::StocksController < ApplicationController
     symbol = params[:id]
     # debugger
     # daycharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=1&interval=1&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+        # weekcharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=7&interval=5&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # monthcharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=30&interval=5&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+
     # @singleDay = RestClient.get(daycharturl)
     # if @singleDay && @stock
     #   render json: {stock: @stock, singleDay: @singleDay}
--- frontend/components/stocks/stock_chart.jsx
+++ frontend/components/stocks/stock_chart.jsx
@@ -4,6 +4,12 @@ import { Line, LineChart, YAxis, XAxis, Tooltip, ReferenceLIne } from 'recharts'
 class StockChart extends React.Components{
   constructor(props) {
     super(props)
+    this.state = { currentSelection: "1d" };
+  }
+
+  render() {
+    const choice = this.state.currentSelection;
+    let data =
   }
 }
 
