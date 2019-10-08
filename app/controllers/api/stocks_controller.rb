edf4486fb9594cea363e1634e865e47bffd8af8f
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-07 23:17:15 -0700

    outline stock chart

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -9,23 +9,22 @@ class Api::StocksController < ApplicationController
     year_time = (Date.today - 365).strftime("%Y-%m-%d") 
     five_year_time = (Date.today - 1825).strftime("%Y-%m-%d") 
     # debugger
-    daycharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=1&interval=1&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
-    weekcharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=7&interval=5&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
-    monthcharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=30&interval=5&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
-    threemonthurl ="https://api.worldtradingdata.com/api/v1/history?symbol=#{symbol}&date_from=#{three_month_time}&date_to=#{today}&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
-    oneyearurl = "https://api.worldtradingdata.com/api/v1/history?symbol=#{symbol}&date_from=#{year_time}&date_to=#{today}&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
-    fiveryearurl = "https://api.worldtradingdata.com/api/v1/history?symbol=#{symbol}&date_from=#{five_year_time}&date_to=#{today}&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
-    @singleDay = RestClient.get(daycharturl)
-    @oneweek = RestClient.get(weekcharturl)
-    @month = RestClient.get(monthcharturl)
-    @threemonth = RestClient.get(threemonthurl)
-    @year = RestClient.get(oneyearurl)
-    @fiveyear = RestClient.get(fiveryearurl)
-    # debugger
-    if @singleDay && @stock && @oneweek && @month && @threemonth && @year && @fiveyear 
-      render json: {stock: @stock, charts: {oneday: @singleDay, oneweek: @oneweek, onemonth: @month, threemonths: @threemonth, oneyear: @year, fiveyears: @fiveyear}}
-    # if @stock 
-    #   render json: {stock: @stock, singleDay: @singleDay}
+    # daycharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=1&interval=1&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # weekcharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=7&interval=5&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # monthcharturl = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=#{symbol}&range=30&interval=5&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # threemonthurl ="https://api.worldtradingdata.com/api/v1/history?symbol=#{symbol}&date_from=#{three_month_time}&date_to=#{today}&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # oneyearurl = "https://api.worldtradingdata.com/api/v1/history?symbol=#{symbol}&date_from=#{year_time}&date_to=#{today}&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # fiveryearurl = "https://api.worldtradingdata.com/api/v1/history?symbol=#{symbol}&date_from=#{five_year_time}&date_to=#{today}&sort=asc&api_token=zWGOqwDCoe0BBKe3FN1SM3x1ahCMbEs47EywsN8rpHTEByE7dMrezhsqBv4A"
+    # @singleDay = RestClient.get(daycharturl)
+    # @oneweek = RestClient.get(weekcharturl)
+    # @month = RestClient.get(monthcharturl)
+    # @threemonth = RestClient.get(threemonthurl)
+    # @year = RestClient.get(oneyearurl)
+    # @fiveyear = RestClient.get(fiveryearurl)
+    # if @singleDay && @stock && @oneweek && @month && @threemonth && @year && @fiveyear 
+    #   render json: {stock: @stock, charts: {oneday: @singleDay, oneweek: @oneweek, onemonth: @month, threemonths: @threemonth, oneyear: @year, fiveyears: @fiveyear}}
+    if @stock 
+      render json: {stock: @stock, singleDay: @singleDay}
     else
       render json: {}, status: :not_found
     end
--- frontend/components/stocks/stock_chart.jsx
+++ frontend/components/stocks/stock_chart.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import { Line, LineChart, YAxis, XAxis, Tooltip, ReferenceLIne } from 'recharts'
+import React from "react";
+import {
+  Line,
+  LineChart,
+  YAxis,
+  XAxis,
+  Tooltip,
+  ReferenceLIne
+} from "recharts";
 
-class StockChart extends React.Components{
+class StockChart extends React.Components {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = { currentSelection: "1d" };
   }
 
   render() {
     const choice = this.state.currentSelection;
-    let data =
+    return (
+      <div>
+        <div id="stock-chart">
+          <h1 id="stock-name">{this.props.name}</h1>
+          {chartOrLoading}
+        </div>
+        <nav id="chart-buttons">
+          {["1d", "1w", "1m", "3m", "1y", "5y"].map(range => {
+            return (
+              <button
+                key={range}
+                className={this.state.currentChart === range ? "selected" : ""}
+                onClick={() => this.setState({ currentChart: range })}
+              >
+                {range}
+              </button>
+            );
+          })}
+        </nav>
+      </div>
+    );
   }
 }
 
-export default StockChart
+export default StockChart;
