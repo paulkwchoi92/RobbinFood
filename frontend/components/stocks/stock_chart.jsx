2321b49819f88590384a84dabfee59bd9d3f789a
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-08 12:06:28 -0700

    adding chart info

--- frontend/components/stocks/stock_chart.jsx
+++ frontend/components/stocks/stock_chart.jsx
@@ -8,10 +8,14 @@ import {
   ReferenceLIne
 } from "recharts";
 
+import ChartTooltip from './stock_chart_toolltip'
+
 class StockChart extends React.Components {
   constructor(props) {
     super(props);
     this.state = { currentSelection: "1d" };
+
+    this.filterData = this.filterData.bind(this)
   }
 
   selectionParser(str) { 
@@ -32,17 +36,51 @@ class StockChart extends React.Components {
         return
     }
   }
+  filterData(data) {
+    const {currentSelection} = this.state
+    if (currentSelection === "1d" || currentSelection === "1w" ||
+      currentSelection === "1m"
+    ) {
+      return data.intraday 
+      } else { return data.history}
+  }
 
   render() {
     const {currentSelection} = this.state
     const choice = this.selectionParser(this.state.currentSelection);
-    let data = this.props.charts[choice]
+    let data = this.filterData(this.props.charts[choice])
     let start, end, currentStocks;
     const value = currentSelection === "1d" ? "open" : "close"
     if (data) {
       currentStocks = data.filter(ele => ele[value])
     }
-    
+    const chartOrLoading = data ? (
+      <LineChart width={676} height={196} data={data}>
+        <Line
+          activeDot={{ r: 6 }}
+          type="linear"
+          strokeWidth={2}
+          dataKey={value}
+          stroke="#21ce99"
+          dot={false}
+        />
+        <Tooltip
+          wrapperStyle={{ visibility: 'visible' }}
+          position={{ x: 0, y: -84 }}
+          content={<ChartTooltip
+            value={end ? end[value] : 0}
+            prev={currentSelection === "1d" ? this.props.prev : (data[0] ? data[0].open : null)}
+          />}
+        />
+        <ReferenceLine className={currentSelection === "1d" ? "" : "hidden"} y={this.props.prev} />
+        <YAxis
+          hide
+          domain={[start, 'dataMax']} />
+        <XAxis
+          hide
+          dataKey="date" />
+      </LineChart>) : <div/>
+      // <Loading id={"loading-chart"} />
     return (
       <div>
         <div id="stock-chart">
