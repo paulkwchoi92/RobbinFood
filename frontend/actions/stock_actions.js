195b981d7afede47c37f89ac741fd60fd012c1cb
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-09 07:55:09 -0700

    front end action creators

--- frontend/actions/stock_actions.js
+++ frontend/actions/stock_actions.js
@@ -4,6 +4,12 @@ export const RECEIVE_ONE_STOCK = "RECEIVE_ONE_STOCK";
 export const RECEIVE_MANY_STOCKS = "RECEIVE_MANY_STOCKS";
 export const RECEIVE_STOCK_ERRORS = "RECEIVE_STOCK_ERRORS";
 export const RECEIVE_ALL_STOCKS = "RECEIVE_ALL_STOCKS";
+export const RECEIVE_TRANSACTION = "RECEIVE_TRANSACTION";
+export const RECEIVE_TRANSACTION_ERRORS = "RECEIVE_TRANSACTION_ERRORS";
+export const RECEIVE_WATCH = "RECEIVE_WATCH";
+export const DELETE_WATCH = "DELETE_WATCH";
+
+// action creators
 export const receiveOneStock = stock => ({
   type: RECEIVE_ONE_STOCK,
   stock
@@ -24,6 +30,23 @@ export const receiveAllStocks = stocks => ({
   stocks
 });
 
+export const receiveTransaction = payload => ({
+  type: RECEIVE_TRANSACTION,
+  payload
+});
+
+export const receiveWatch = watch => ({
+  type: RECEIVE_WATCH,
+  watch
+});
+
+export const deleteWatch = id => ({
+  type: DELETE_WATCH,
+  id
+});
+
+//dispatch actions
+
 export const fetchAllStocks = () => dispatch => {
   return (
     StockAPIUtil.fetchAllStocks().then(stocks =>
@@ -42,4 +65,18 @@ export const fetchStock = symbol => dispatch => {
     }),
     err => dispatch(receiveStockErrors(err))
   );
+};
+
+
+export const makeTransaction = transaction => dispatch => {
+  return APIUtil.makeTransaction(transaction).then(payload => dispatch(receiveTransaction(payload)),
+    ({ responseJSON }) => dispatch(receiveErrors(responseJSON)));
+};
+
+export const watchStock = (id, symbol) => dispatch => {
+  return APIUtil.watchStock(id, symbol).then(watch => dispatch(receiveWatch(watch)));
+};
+
+export const removeWatch = id => dispatch => {
+  return APIUtil.deleteWatch(id).then(({ id }) => dispatch(deleteWatch(id)));
 };
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,8 +3,12 @@ import { combineReducers } from "redux";
 import users from "./user_reducer";
 import stocks from "./stocks_reducer";
 import news from "./news_reducer"
+import watchlists from "./watchlist_reducer",
+import transactions from "./transaction_reducer"
 export default combineReducers({
   users,
   stocks,
-  news
+  news,
+  watchlists,
+  transactions
 });
